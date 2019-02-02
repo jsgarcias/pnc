f9c57f74bf2da1d371aa1ba12629826474772fdb
juanpaguti <36583825+juanpaguti@users.noreply.github.com>
2019-02-02 17:50:50 -0600

    pantalla informate para personalizar, imgs que hacemos y drop prod, servicios

--- js/info.js
+++ js/info.js
@@ -1,1 +1,24 @@
-﻿
+﻿function MuestraDivInsert1() {
+    $('#divinsert1').toggle();
+
+}
+
+function OcultaDivInsert1() {
+    $('#divinsert1').toggle();
+
+}
+
+function MuestraDivInsert2() {
+    $('#divinsert2').toggle();
+ 
+}
+
+function MuestraDivInsert3() {
+    $('#divinsert3').toggle();
+
+}
+
+function MuestraDivInsert4() {
+    $('#divinsert4').toggle();
+  
+}
